887004304ba2034cb670c9a6153934e416d89e64
Algebrah <s0043581@rgare.net>
2016-08-17 09:43:51 -0500

    Added REST Injections

--- src/main/java/com/bharaththippireddy/trainings/jaxrs/PassengerService.java
+++ src/main/java/com/bharaththippireddy/trainings/jaxrs/PassengerService.java
@@ -1,13 +1,22 @@
 package com.bharaththippireddy.trainings.jaxrs;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.eclipse.jetty.server.Request;
 
 import com.bharathippireddy.trainings.jaxrs.dto.Passenger;
 import com.bharathippireddy.trainings.jaxrs.dto.PassengerList;
@@ -32,16 +41,31 @@ public class PassengerService {
 	}
 
 	@GET
-	public PassengerList getPassengers(@QueryParam("start") int start, @QueryParam("size") int size, String agent,
-			HttpHeaders headers) {
+	public PassengerList getPassengers(@QueryParam("start") int start, @QueryParam("size") int size, @HeaderParam("agent") String agent,
+			@Context HttpHeaders headers) {
+		MultivaluedMap<String, String> requestHeaders = headers.getRequestHeaders();
+		Set<String> headerNameSet = requestHeaders.keySet();
+		
+		Map<String, Cookie> cookies = headers.getCookies();
+		Set<String> names = cookies.keySet();
+		for(String name : names){
+			cookies.get(name);
+			System.out.println(names);
+		}
+		
+		for (String eachHeader : headerNameSet) {
+			System.out.println(headers.getHeaderString(eachHeader));
+			}
+		
 		System.out.println(start);
 		System.out.println(size);
 		return passengersList;
 	}
 
 	@POST
-	public void addPassenger(String firstName, String lastName) {
-
+	public void addPassenger(@FormParam("firstname") String firstName,@FormParam("lastname") String lastName) {
+		System.out.print(firstName);
+		System.out.print(lastName);	
 	}
 
 }
